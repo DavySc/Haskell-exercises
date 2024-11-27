d3aa7609c4b2577b4fac620c5e3974b2f3583675
Davy <d.vdschoot@gmail.com>
2024-11-27 11:31:01 +0100

    refactor

--- effective-haskell/functions-as-values/velocity.hs
+++ effective-haskell/functions-as-values/velocity.hs
@@ -1,10 +1,16 @@
 module Velocity where
 
-velocity :: Double -> Double -> Double
+type Meters = Double
+
+type Seconds = Double
+
+type MetersPerSecond = Double
+
+velocity :: Meters -> Seconds -> MetersPerSecond
 velocity meters seconds = meters / seconds
 
-speedLimit :: Double
+speedLimit :: MetersPerSecond
 speedLimit =
-  let meters = 299792458 :: Double
-      seconds = 1.0 :: Double
+  let meters = 299792458 :: Meters
+      seconds = 1.0 :: Seconds
    in velocity meters seconds
