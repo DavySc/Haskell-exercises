1447efd83aa677942653a4796113e31a032ee3cf
Davy <dvdschoot@sogyo.nl>
2024-12-10 22:43:55 +0100

    Rename and test

--- effective-haskell/calculator.hs
+++ effective-haskell/calculator.hs
@@ -15,17 +15,17 @@ eval expr = case expr of
 safeEval :: Expr -> Either String Int
 safeEval expr = case expr of
   Lit num -> Right num
-  Add arg1 arg2 -> eval' (ophelper (+)) arg1 arg2
-  Sub arg1 arg2 -> eval' (ophelper (-)) arg1 arg2
-  Mul arg1 arg2 -> eval' (ophelper (*)) arg1 arg2
+  Add arg1 arg2 -> eval' (opHelper (+)) arg1 arg2
+  Sub arg1 arg2 -> eval' (opHelper (-)) arg1 arg2
+  Mul arg1 arg2 -> eval' (opHelper (*)) arg1 arg2
   Div arg1 arg2 -> eval' safeDiv arg1 arg2
   where
     safeDiv a b
       | b == 0 = Left "Error: Division by zero"
       | otherwise = Right $ a `div` b
 
-    ophelper :: (Int -> Int -> Int) -> Int -> Int -> Either String Int
-    ophelper op a b = Right $ a `op` b
+    opHelper :: (Int -> Int -> Int) -> Int -> Int -> Either String Int
+    opHelper op a b = Right $ a `op` b
     eval' :: (Int -> Int -> Either String Int) -> Expr -> Expr -> Either String Int
     eval' operator arg1 arg2 =
       case safeEval arg1 of
